ACINQ/lightning-kmp

Add support for Bolt 12 contacts
Add support for contacts as specified in bLIP 42. Contacts are mutually
authenticated using a 32-bytes random secret generated when first adding
a node to our contacts. When paying contacts, we include our own payment
information to allow them to pay us back and us to their contacts.

The benefit of this design is that offers stay private by default (they
don't include any contact information). It's only when we pay someone
we trust that we reveal contact information (which they are free to
ignore).

The drawback of this design is that if when both nodes independently add
each other to their contacts list, they generate a different contact
secret: users must manually associate incoming payments to an existing
contact to correctly identify incoming payments (by storing multiple
secrets for such contacts). This also happens when contacts use multiple
wallets, which will all use different contact secrets. I think this is
an acceptable trade-off to preserve privacy by default.

More details in the bLIP: https://github.com/lightning/blips/pull/42

src/commonTest/kotlin/fr/acinq/lightning/payment/OfferPaymentMetadataTestsCommon.kt
@@ -1,14 +1,24 @@
 package fr.acinq.lightning.payment
 
 import fr.acinq.bitcoin.ByteVector
+import fr.acinq.bitcoin.Crypto
+import fr.acinq.bitcoin.byteVector32
+import fr.acinq.lightning.Feature
+import fr.acinq.lightning.FeatureSupport
+import fr.acinq.lightning.Features
 import fr.acinq.lightning.Lightning.randomBytes32
 import fr.acinq.lightning.Lightning.randomKey
+import fr.acinq.lightning.tests.TestConstants
 import fr.acinq.lightning.utils.msat
+import fr.acinq.lightning.wire.OfferTypes
+import kotlin.experimental.xor
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 class OfferPaymentMetadataTestsCommon {
+
     @Test
     fun `encode - decode v1 metadata`() {
         val nodeKey = randomKey()
@@ -23,7 +33,7 @@ class OfferPaymentMetadataTestsCommon {
         )
         assertEquals(metadata, OfferPaymentMetadata.decode(metadata.encode()))
         val pathId = metadata.toPathId(nodeKey)
-        assertEquals(metadata, OfferPaymentMetadata.fromPathId(nodeKey.publicKey(), pathId))
+        assertEquals(metadata, OfferPaymentMetadata.fromPathId(nodeKey, pathId, Crypto.sha256(metadata.preimage).byteVector32()))
     }
 
     @Test
@@ -40,30 +50,108 @@ class OfferPaymentMetadataTestsCommon {
         )
         assertEquals(metadata, OfferPaymentMetadata.decode(metadata.encode()))
         val pathId = metadata.toPathId(nodeKey)
-        assertEquals(metadata, OfferPaymentMetadata.fromPathId(nodeKey.publicKey(), pathId))
+        assertEquals(metadata, OfferPaymentMetadata.fromPathId(nodeKey, pathId, Crypto.sha256(metadata.preimage).byteVector32()))
+    }
+
+    @Test
+    fun `encode - decode v2 metadata`() {
+        val nodeKey = randomKey()
+        val preimage = randomBytes32()
+        val paymentHash = Crypto.sha256(preimage).byteVector32()
+        val metadata = OfferPaymentMetadata.V2(
+            offerId = randomBytes32(),
+            amount = 200_000_000.msat,
+            preimage = preimage,
+            payerKey = randomKey().publicKey(),
+            payerNote = "thanks for all the fish",
+            quantity = 1,
+            contactSecret = null,
+            payerOffer = null,
+            payerAddress = null,
+            createdAtMillis = 0
+        )
+        assertEquals(metadata, OfferPaymentMetadata.decode(metadata.encode()))
+        val pathId = metadata.toPathId(nodeKey)
+        assertTrue(pathId.size() in 150..200)
+        assertEquals(metadata, OfferPaymentMetadata.fromPathId(nodeKey, pathId, paymentHash))
+    }
+
+    @Test
+    fun `encode - decode v2 metadata with contact information`() {
+        val nodeKey = randomKey()
+        val preimage = randomBytes32()
+        val paymentHash = Crypto.sha256(preimage).byteVector32()
+        val payerOffer = OfferTypes.Offer.createBlindedOffer(
+            amount = null,
+            description = null,
+            nodeParams = TestConstants.Alice.nodeParams,
+            trampolineNodeId = randomKey().publicKey(),
+            features = Features(
+                Feature.VariableLengthOnion to FeatureSupport.Optional,
+                Feature.PaymentSecret to FeatureSupport.Optional,
+                Feature.BasicMultiPartPayment to FeatureSupport.Optional,
+            ),
+            blindingSecret = randomKey()
+        ).offer
+        val metadata = OfferPaymentMetadata.V2(
+            offerId = randomBytes32(),
+            amount = 200_000_000.msat,
+            preimage = preimage,
+            payerKey = randomKey().publicKey(),
+            payerNote = "hello there",
+            quantity = 1,
+            contactSecret = randomBytes32(),
+            payerOffer = payerOffer,
+            payerAddress = null,
+            createdAtMillis = 0
+        )
+        assertEquals(metadata, OfferPaymentMetadata.decode(metadata.encode()))
+        val pathId = metadata.toPathId(nodeKey)
+        assertTrue(pathId.size() in 400..450)
+        assertEquals(metadata, OfferPaymentMetadata.fromPathId(nodeKey, pathId, paymentHash))
     }
 
     @Test
     fun `decode invalid path_id`() {
         val nodeKey = randomKey()
-        val metadata = OfferPaymentMetadata.V1(
+        val preimage = randomBytes32()
+        val paymentHash = Crypto.sha256(preimage).byteVector32()
+        val metadataV1 = OfferPaymentMetadata.V1(
             offerId = randomBytes32(),
             amount = 50_000_000.msat,
-            preimage = randomBytes32(),
+            preimage = preimage,
             payerKey = randomKey().publicKey(),
             payerNote = null,
             quantity = 1,
             createdAtMillis = 0
         )
+        val metadataV2 = OfferPaymentMetadata.V2(
+            offerId = randomBytes32(),
+            amount = 50_000_000.msat,
+            preimage = preimage,
+            payerKey = randomKey().publicKey(),
+            payerNote = null,
+            quantity = 1,
+            contactSecret = randomBytes32(),
+            payerOffer = null,
+            payerAddress = null,
+            createdAtMillis = 0
+        )
         val testCases = listOf(
             ByteVector.empty,
             ByteVector("02deadbeef"), // invalid version
-            metadata.toPathId(nodeKey).dropRight(1), // not enough bytes
-            metadata.toPathId(nodeKey).concat(ByteVector("deadbeef")), // too many bytes
-            metadata.toPathId(randomKey()), // signed with different key
+            metadataV1.toPathId(nodeKey).dropRight(1), // not enough bytes
+            metadataV1.toPathId(nodeKey).concat(ByteVector("deadbeef")), // too many bytes
+            metadataV1.toPathId(randomKey()), // signed with different key
+            metadataV2.toPathId(nodeKey).drop(1), // missing bytes at the start
+            metadataV2.toPathId(nodeKey).dropRight(1), // missing bytes at the end
+            metadataV2.toPathId(nodeKey).let { it.update(13, it[13].xor(0xff.toByte())) }, // modified bytes
+            metadataV2.toPathId(nodeKey).concat(ByteVector("deadbeef")), // additional bytes
+            metadataV2.toPathId(randomKey()), // encrypted with different key
         )
         testCases.forEach {
-            assertNull(OfferPaymentMetadata.fromPathId(nodeKey.publicKey(), it))
+            assertNull(OfferPaymentMetadata.fromPathId(nodeKey, it, paymentHash))
         }
     }
+
 }

src/commonTest/kotlin/fr/acinq/lightning/payment/PaymentPacketTestsCommon.kt
@@ -459,7 +459,7 @@ class PaymentPacketTestsCommon : LightningTestSuite() {
         // E can correctly decrypt the blinded payment.
         val payloadE = IncomingPaymentPacket.decrypt(addE, privE).right!!
         assertIs<PaymentOnion.FinalPayload.Blinded>(payloadE)
-        val paymentMetadata = OfferPaymentMetadata.fromPathId(e, payloadE.pathId)
+        val paymentMetadata = OfferPaymentMetadata.fromPathId(privE, payloadE.pathId, addE.paymentHash)
         assertNotNull(paymentMetadata)
         assertEquals(offer.offerId, paymentMetadata.offerId)
         assertEquals(paymentMetadata.paymentHash, invoice.paymentHash)

ACINQ/eclair-kmp

modules/core/src/commonTest/kotlin/fr/acinq/lightning/payment/OfferPaymentMetadataTestsCommon.kt
@@ -1,14 +1,24 @@
 package fr.acinq.lightning.payment
 
 import fr.acinq.bitcoin.ByteVector
+import fr.acinq.bitcoin.Crypto
+import fr.acinq.bitcoin.byteVector32
+import fr.acinq.lightning.Feature
+import fr.acinq.lightning.FeatureSupport
+import fr.acinq.lightning.Features
 import fr.acinq.lightning.Lightning.randomBytes32
 import fr.acinq.lightning.Lightning.randomKey
+import fr.acinq.lightning.tests.TestConstants
 import fr.acinq.lightning.utils.msat
+import fr.acinq.lightning.wire.OfferTypes
+import kotlin.experimental.xor
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 class OfferPaymentMetadataTestsCommon {
+
     @Test
     fun `encode - decode v1 metadata`() {
         val nodeKey = randomKey()
@@ -23,7 +33,7 @@ class OfferPaymentMetadataTestsCommon {
         )
         assertEquals(metadata, OfferPaymentMetadata.decode(metadata.encode()))
         val pathId = metadata.toPathId(nodeKey)
-        assertEquals(metadata, OfferPaymentMetadata.fromPathId(nodeKey.publicKey(), pathId))
+        assertEquals(metadata, OfferPaymentMetadata.fromPathId(nodeKey, pathId, Crypto.sha256(metadata.preimage).byteVector32()))
     }
 
     @Test
@@ -40,30 +50,108 @@ class OfferPaymentMetadataTestsCommon {
         )
         assertEquals(metadata, OfferPaymentMetadata.decode(metadata.encode()))
         val pathId = metadata.toPathId(nodeKey)
-        assertEquals(metadata, OfferPaymentMetadata.fromPathId(nodeKey.publicKey(), pathId))
+        assertEquals(metadata, OfferPaymentMetadata.fromPathId(nodeKey, pathId, Crypto.sha256(metadata.preimage).byteVector32()))
+    }
+
+    @Test
+    fun `encode - decode v2 metadata`() {
+        val nodeKey = randomKey()
+        val preimage = randomBytes32()
+        val paymentHash = Crypto.sha256(preimage).byteVector32()
+        val metadata = OfferPaymentMetadata.V2(
+            offerId = randomBytes32(),
+            amount = 200_000_000.msat,
+            preimage = preimage,
+            payerKey = randomKey().publicKey(),
+            payerNote = "thanks for all the fish",
+            quantity = 1,
+            contactSecret = null,
+            payerOffer = null,
+            payerAddress = null,
+            createdAtMillis = 0
+        )
+        assertEquals(metadata, OfferPaymentMetadata.decode(metadata.encode()))
+        val pathId = metadata.toPathId(nodeKey)
+        assertTrue(pathId.size() in 150..200)
+        assertEquals(metadata, OfferPaymentMetadata.fromPathId(nodeKey, pathId, paymentHash))
+    }
+
+    @Test
+    fun `encode - decode v2 metadata with contact information`() {
+        val nodeKey = randomKey()
+        val preimage = randomBytes32()
+        val paymentHash = Crypto.sha256(preimage).byteVector32()
+        val payerOffer = OfferTypes.Offer.createBlindedOffer(
+            amount = null,
+            description = null,
+            nodeParams = TestConstants.Alice.nodeParams,
+            trampolineNodeId = randomKey().publicKey(),
+            features = Features(
+                Feature.VariableLengthOnion to FeatureSupport.Optional,
+                Feature.PaymentSecret to FeatureSupport.Optional,
+                Feature.BasicMultiPartPayment to FeatureSupport.Optional,
+            ),
+            blindedPathSessionKey = randomKey()
+        ).offer
+        val metadata = OfferPaymentMetadata.V2(
+            offerId = randomBytes32(),
+            amount = 200_000_000.msat,
+            preimage = preimage,
+            payerKey = randomKey().publicKey(),
+            payerNote = "hello there",
+            quantity = 1,
+            contactSecret = randomBytes32(),
+            payerOffer = payerOffer,
+            payerAddress = null,
+            createdAtMillis = 0
+        )
+        assertEquals(metadata, OfferPaymentMetadata.decode(metadata.encode()))
+        val pathId = metadata.toPathId(nodeKey)
+        assertTrue(pathId.size() in 400..450)
+        assertEquals(metadata, OfferPaymentMetadata.fromPathId(nodeKey, pathId, paymentHash))
     }
 
     @Test
     fun `decode invalid path_id`() {
         val nodeKey = randomKey()
-        val metadata = OfferPaymentMetadata.V1(
+        val preimage = randomBytes32()
+        val paymentHash = Crypto.sha256(preimage).byteVector32()
+        val metadataV1 = OfferPaymentMetadata.V1(
             offerId = randomBytes32(),
             amount = 50_000_000.msat,
-            preimage = randomBytes32(),
+            preimage = preimage,
             payerKey = randomKey().publicKey(),
             payerNote = null,
             quantity = 1,
             createdAtMillis = 0
         )
+        val metadataV2 = OfferPaymentMetadata.V2(
+            offerId = randomBytes32(),
+            amount = 50_000_000.msat,
+            preimage = preimage,
+            payerKey = randomKey().publicKey(),
+            payerNote = null,
+            quantity = 1,
+            contactSecret = randomBytes32(),
+            payerOffer = null,
+            payerAddress = null,
+            createdAtMillis = 0
+        )
         val testCases = listOf(
             ByteVector.empty,
             ByteVector("02deadbeef"), // invalid version
-            metadata.toPathId(nodeKey).dropRight(1), // not enough bytes
-            metadata.toPathId(nodeKey).concat(ByteVector("deadbeef")), // too many bytes
-            metadata.toPathId(randomKey()), // signed with different key
+            metadataV1.toPathId(nodeKey).dropRight(1), // not enough bytes
+            metadataV1.toPathId(nodeKey).concat(ByteVector("deadbeef")), // too many bytes
+            metadataV1.toPathId(randomKey()), // signed with different key
+            metadataV2.toPathId(nodeKey).drop(1), // missing bytes at the start
+            metadataV2.toPathId(nodeKey).dropRight(1), // missing bytes at the end
+            metadataV2.toPathId(nodeKey).let { it.update(13, it[13].xor(0xff.toByte())) }, // modified bytes
+            metadataV2.toPathId(nodeKey).concat(ByteVector("deadbeef")), // additional bytes
+            metadataV2.toPathId(randomKey()), // encrypted with different key
         )
         testCases.forEach {
-            assertNull(OfferPaymentMetadata.fromPathId(nodeKey.publicKey(), it))
+            assertNull(OfferPaymentMetadata.fromPathId(nodeKey, it, paymentHash))
         }
     }
+
 }

modules/core/src/commonTest/kotlin/fr/acinq/lightning/payment/PaymentPacketTestsCommon.kt
@@ -365,7 +365,7 @@ class PaymentPacketTestsCommon : LightningTestSuite() {
         assertIs<PaymentOnion.FinalPayload.Blinded>(payloadD)
         assertEquals(finalAmount, payloadD.amount)
         assertEquals(finalExpiry, payloadD.expiry)
-        val paymentMetadata = OfferPaymentMetadata.fromPathId(d, payloadD.pathId)
+        val paymentMetadata = OfferPaymentMetadata.fromPathId(privD, payloadD.pathId, addD.paymentHash)
         assertNotNull(paymentMetadata)
         assertEquals(offer.offerId, paymentMetadata.offerId)
         assertEquals(paymentMetadata.paymentHash, invoice.paymentHash)
@@ -390,7 +390,7 @@ class PaymentPacketTestsCommon : LightningTestSuite() {
         assertEquals(finalAmount, payloadD.amount)
         assertEquals(finalAmount, payloadD.totalAmount)
         assertEquals(finalExpiry, payloadD.expiry)
-        assertEquals(paymentMetadata, OfferPaymentMetadata.fromPathId(d, payloadD.pathId))
+        assertEquals(paymentMetadata, OfferPaymentMetadata.fromPathId(privD, payloadD.pathId, addD.paymentHash))
     }
 
     @Test